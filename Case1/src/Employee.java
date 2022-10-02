5a1f3fa4521b0c199c7f682e5cd5e0f8d2cc72df
plaxia <plaxia13@gmail.com>
2022-10-02 15:08:24 +0300

    first commit

---
+++ Case1/src/Employee.java
@@ -0,0 +1,88 @@
+public class Employee {
+    String name;
+    double salary;
+    int workHours;
+    int hireYear;
+     Employee(String name,double salary,int workHours,int hireYear)
+    {
+        this.name=name;
+        this.salary=salary;
+        this.workHours=workHours;
+        this.hireYear=hireYear;
+
+    }
+    public double tax()
+    {
+        if (salary<=0)
+        {
+            System.out.println("Hatali bir deger girdiniz vergi hesaplanamiyor.");
+        }
+
+
+
+        if(salary<=1000)
+        {
+        return salary;
+        }
+        else
+        {
+            double tax = salary*0.03;
+            return tax;
+        }
+
+    }
+    public double bonus()
+    {
+        if(workHours<=0){
+            System.out.println("Hatali bir deger girdiniz bonus hesaplanamiyor.");
+        }
+        if(workHours>40)
+        {
+            int bonus=(workHours-40)*30;
+            return bonus;
+        }
+        else return 0;
+    }
+    public double raiseSalary()
+    {
+        if(hireYear<0||hireYear==2021||hireYear==0){
+            System.out.println("Hatali bir deger girdiniz, maas artisi hesaplanamiyor.");
+        }
+        else {
+
+
+        int wYear=2021-hireYear;
+        if(wYear<10)
+        {
+            double zam=salary*0.05;
+           return zam;
+        }
+        else if (wYear>9||wYear<20)
+        {
+            double zam=salary*0.1;
+            return zam;
+        }
+        else if (2021-hireYear>19)
+        {
+            double zam=salary*0.15;
+            return zam;
+        }
+        }
+        return 0;
+    }
+
+    public String toString()
+    {
+        double total=salary-tax()+bonus();
+        double toplam=salary-tax()+bonus()+raiseSalary();
+        System.out.println("Adi:"+name);
+        System.out.println("Maas:"+salary);
+        System.out.println("Baslangic Yili:"+hireYear);
+        System.out.println("Vergi:"+tax()+" TL");
+        System.out.println("Bonus:"+bonus()+" TL");
+        System.out.println("Maas artisi:"+raiseSalary()+" TL");
+        System.out.println("Vergi ve bonuslarla birlikte maas:"+total+" TL");
+        System.out.println("Toplam maas:"+toplam+" TL");
+        return null;
+    }
+}
---
+++ Case1/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee e = new Employee("Berke",20000,50,1990);
+        e.toString();
+    }
+}
